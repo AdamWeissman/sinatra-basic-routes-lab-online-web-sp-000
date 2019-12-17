0d8b0a690206e711567c50c899bcfd0ea8a9e2a8
Adam Weissman <adam@comedytragedyepic.com>
2019-12-17 10:42:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,26 +1,30 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
-  def self.one_for_all(arg1, arg2, arg3)
-    get arg1 do
-      "My name is Adam"
-    end
+
+  def self.one_for_all(*args)
+    args.each do 
+      
+ 
+  #def self.one_for_all(arg1, arg2, arg3)
+  #  get arg1 do
+  #    "My name is Adam"
+  #  end
     
-    get arg2 do
-      "My hometown is Eastchester"
-    end
+  #  get arg2 do
+  #    "My hometown is Eastchester"
+  #  end
     
-    get arg3 do
-      "My favorite song is __"
-    end
-  end
+  #  get arg3 do
+  #    "My favorite song is __"
+  #  end
+  #end
   
   one_for_all("/name", "/hometown", "/favorite-song")
     
   
   
-  #get '/name', '/hometown', '/favorite-song' do
+  #get '/name', '/hometown', '/favorite-song' do THIS DOESNT WORK!!!
   #  "My name is Adam"
   #  "My hometown is Eastchester"
   #  "My favorite-song is New York, New York"
