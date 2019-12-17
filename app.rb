59efa281e06c4e40b61571db6cc0bebdbd9cfa52
Adam Weissman <adam@comedytragedyepic.com>
2019-12-17 10:35:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,11 +2,30 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/name', '/hometown', '/favorite-song' do
-    "My name is Adam"
-    "My hometown is Eastchester"
-    "My favorite-song is New York, New York"
+  def one_for_all(arg1, arg2, arg3)
+    get arg1 do
+      "My name is Adam"
+    end
+    
+    get arg2 do
+      "My hometown is Eastchester"
+    end
+    
+    get arg3 do
+      "My favorite-song is New York New York"
+    end
   end
+  
+  one_for_all("name", "hometown", "favorite-song")
+
+    
+  
+  
+  #get '/name', '/hometown', '/favorite-song' do
+  #  "My name is Adam"
+  #  "My hometown is Eastchester"
+  #  "My favorite-song is New York, New York"
+  #end
   
   
 end
