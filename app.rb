76d51488ad920b9a4244006f2b795477c7a7dad2
Adam Weissman <adam@comedytragedyepic.com>
2019-12-17 10:31:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name', '/hometown', '/favorite-song' do
+    "My name is Adam"
+    "My hometown is Eastchester"
+    "My favorite-song is New York, New York"
+  end
+  
+  
 end
